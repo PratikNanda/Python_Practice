881e0b53e3af7447fbc55f9a14e084c8e51805da
PratikNanda <87570208+PratikNanda@users.noreply.github.com>
2023-09-10 08:19:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+global val
+def driver():
+    li = ["😊","👍","🐍","💕","😡","🐸"]
+    x = random.choice(li)
+    for num in range (1,105):
+        if (num%72 ==0):
+            print("[",num,"-",x,"]")
+        elif (num%8 == 0):
+            print("[",num,"-",random.choice(li),"]")
+        elif (num%9 ==0):
+            print("[",num,"-",x,"]",end=" ")                    
+        else:
+            print("[",num,"-",random.choice(li),"]",end=" ")
+    print()
+    return x
+val = None
+while(val != "0"):
+    input("Welcome to MIND READER")
+    input("Please choose distinct digits 2 digit number")
+    input("Now reverse the digits of chosen number")
+    input("Now subtract the smaller number with bigger number")
+    input("Memorize the obtained number")
+    input("look at the emoji beside the obtained number")
+    y = driver()
+    input()
+    print("Your emoji is : ",y)
+    val = input("If you dont want to continue then press 0\n")
+    
+
+
